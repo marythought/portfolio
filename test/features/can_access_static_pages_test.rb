b45915fc4915efbabf2e347639d8989a3f85e6e9
Mary Dickson Diaz <mdickson@gmail.com>
2015-09-07 13:46:07 -0700

    test tweaks

--- test/features/can_access_static_pages_test.rb
+++ test/features/can_access_static_pages_test.rb
@@ -6,12 +6,4 @@ feature "CanAccessStaticPages" do
     page.must_have_content "Mary Dickson Diaz"
     page.wont_have_content "Goodbye All!"
   end
-
-  scenario "on a custom domain" do
-    visit("http://www.marydickson.info")
-    page.must_have_content("Mary Dickson Diaz")
-    # This is passing but it also passes with inaccurate content
-    # So I think it's broken.
-    # But I don't know why :-(
-  end
 end
