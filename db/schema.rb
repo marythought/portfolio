f5145ebe0a7bd65bdd6f6fd38d8f347d8a3f31e2
Mary Dickson Diaz <mdickson@gmail.com>
2015-09-14 15:42:48 -0700

    add image_url to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913012126) do
+ActiveRecord::Schema.define(version: 20150914224132) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150913012126) do
     t.datetime "updated_at",                 null: false
     t.integer  "user_id"
     t.boolean  "published",  default: false
+    t.string   "image_url"
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
