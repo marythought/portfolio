c1e6ca30fec5bcf81872f8e555dd2e30d4c46651
Mary Dickson Diaz <mdickson@gmail.com>
2015-11-20 21:49:31 -0800

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
-  resources :articles do
-    resources :comments do
-    end
-  end
+  devise_for :users
+  # resources :articles do
+  #   resources :comments do
+  #   end
+  # end
 
   resources :projects
   root 'welcome#index'
-  get 'welcome/work'
-  get 'welcome/about'
-  get 'portfolio', to: 'welcome#portfolio'
   get 'about', to: 'welcome#about'
 end
