27ff726780957397049014a2d925687ef60872f3
Mary Dickson Diaz <mdickson@navigatingcancer.com>
2019-09-11 19:54:43 -0700

    [add-react] linting

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { List } from 'semantic-ui-react';
 
+/* global fetch */
 const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
 const GET_THINGS_SUCCESS = 'GET_THINGS_SUCCESS';
 
@@ -24,31 +25,26 @@ function getThings() {
   };
 }
 
-// eslint-disable-next-line react/prefer-stateless-function
-class HelloWorld extends React.Component {
-  render() {
-    const { things } = this.props;
-    const thingsList = things.map((thing) => {
-      return (
-        <List.Item>
-          <List.Icon name="arrow circle right" size="large" verticalAlign="middle" />
-          <List.Content>
-            <List.Header as="a">{thing.name}</List.Header>
-            <List.Description as="a">{thing.guid}</List.Description>
-          </List.Content>
-        </List.Item>
-      );
-    });
+// eslint-disable-next-line no-shadow
+const HelloWorld = ({ things, getThings }) => {
+  const thingsList = things.map((thing) => (
+    <List.Item>
+      <List.Icon name="arrow circle right" size="large" verticalAlign="middle" />
+      <List.Content>
+        <List.Header as="a">{thing.name}</List.Header>
+        <List.Description as="a">{thing.guid}</List.Description>
+      </List.Content>
+    </List.Item>
+  ));
 
-    return (
-      <>
-        <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
-        <br />
-        <List>{ thingsList }</List>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <button type="submit" className="getThingsBtn" onClick={() => getThings()}>getThings</button>
+      <br />
+      <List>{ thingsList }</List>
+    </>
+  );
+};
 
 const structuredSelector = createStructuredSelector({
   things: (state) => state.things,
@@ -63,10 +59,12 @@ HelloWorld.propTypes = {
       name: PropTypes.string,
     }),
   ),
+  getThings: PropTypes.func,
 };
 
 HelloWorld.defaultProps = {
   things: [{ name: 'test', guid: 'abc' }],
+  getThings: () => {},
 };
 
 export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@
 import 'semantic-ui-css/semantic.min.css';
 
 // Support component names relative to this directory:
-var componentRequireContext = require.context('components', true);
-var ReactRailsUJS = require('react_ujs');
+const componentRequireContext = require.context('components', true);
+const ReactRailsUJS = require('react_ujs');
+
 ReactRailsUJS.useContext(componentRequireContext);
