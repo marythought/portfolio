77eea4374d28dc66423f83e06b62379cf803c644
Mary Dickson Diaz <mdickson@navigatingcancer.com>
2019-09-16 14:38:28 -0700

    [commonplace-book] some v basic styling (needs moar)

--- app/javascript/components/Commonplaces/CommonplacesList.jsx
+++ app/javascript/components/Commonplaces/CommonplacesList.jsx
@@ -21,16 +21,22 @@ export default class CommonplacesList extends React.Component {
     const { commonplaces } = this.state;
     const list = commonplaces.map((commonplace) => (
       <List.Item key={commonplace.id}>
-        <List.Icon name="arrow circle right" size="large" verticalAlign="middle" />
+        <List.Icon name="thumbtack" size="large" verticalAlign="middle" />
         <List.Content>
           <List.Header as="a" href={commonplace.url}>{commonplace.quote}</List.Header>
-          <List.Description>{commonplace.notes}</List.Description>
-          <List.Description>{commonplace.source}</List.Description>
+          <List.Description>
+            Source:&nbsp;
+            {commonplace.source}
+          </List.Description>
+          <List.Description>
+            Notes:&nbsp;
+            {commonplace.notes}
+          </List.Description>
         </List.Content>
       </List.Item>
     ));
     return (
-      <List>
+      <List className="very relaxed large">
         {list}
       </List>
     );
