a14a1cc22b3011c9e81d1de04ded3150ba64e09e
Mary Dickson Diaz <mdickson@navigatingcancer.com>
2019-09-24 19:32:59 -0700

    [categories] re-add package lock?

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,15 @@ class User < ActiveRecord::Base
     role.nil?
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.nickname
+      user.email = "#{user.name}-CHANGEME@twitter.example.com"
+    end
+  end
+
   def self.new_with_session(params, session)
     if session['devise.user_attributes']
       new(session['devise.user_attributes'], without_protection: true) do |user|
